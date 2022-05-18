592bfdba516be1f1baca7c0dbeffda2970c300b3
orhanagacli <orhanagacli@gmail.com>
2022-05-18 19:08:23 +0300

    .

--- client/src/components/Author/ListBlogs/BlogsTable.vue
+++ client/src/components/Author/ListBlogs/BlogsTable.vue
@@ -33,7 +33,7 @@
       </v-toolbar>
     </template>
     <template v-slot:item.actions="{ item }">
-      <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+      <v-icon small class="mr-2" @click="editItem(item)"> mdi-help </v-icon>
       <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
     </template>
     <template v-slot:item.createdAt="{ item }">
